7132cd64102cc27257f57d0f259e11ee8e7415a7
RootRobo <47434248+RootRobo@users.noreply.github.com>
2020-04-28 20:01:03 +0200

    new version

--- src/main/java/systems/kinau/fishingbot/FishingBot.java
+++ src/main/java/systems/kinau/fishingbot/FishingBot.java
@@ -31,7 +31,7 @@ import java.util.logging.Logger;
 
 public class FishingBot {
 
-    public static final String PREFIX = "FishingBot v2.6 - ";
+    public static final String PREFIX = "FishingBot v2.6.1 - ";
     @Getter private static FishingBot instance;
     @Getter public static Logger log = Logger.getLogger(FishingBot.class.getSimpleName());
 
