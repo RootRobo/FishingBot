917eed1082ae26530cdf807c2ff9e81eb75cb616
RootRobo <47434248+RootRobo@users.noreply.github.com>
2020-04-28 18:48:10 +0200

    Edit ChatCommandModule

--- src/main/java/systems/kinau/fishingbot/modules/ChatCommandModule.java
+++ src/main/java/systems/kinau/fishingbot/modules/ChatCommandModule.java
@@ -11,6 +11,9 @@ import systems.kinau.fishingbot.event.Listener;
 import systems.kinau.fishingbot.event.play.ChatEvent;
 import systems.kinau.fishingbot.network.protocol.play.PacketOutChat;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class ChatCommandModule extends Module implements Listener {
 
     @Override
@@ -25,8 +28,61 @@ public class ChatCommandModule extends Module implements Listener {
 
     @EventHandler
     public void onChat(ChatEvent event) {
-        if (isEnabled() && event.getText().contains(FishingBot.getInstance().getAuthData().getUsername() + ", Level?")) {
-            FishingBot.getInstance().getNet().sendPacket(new PacketOutChat(FishingBot.getInstance().getPlayer().getLevels() + " Level, Sir!"));
+        if (!isEnabled()) return;
+        String username = FishingBot.getInstance().getAuthData().getUsername();
+        String prefix = username + ".";
+        String message = event.getText();
+        String[] splitMessage = message.split(" ");
+        String sender = splitMessage[0];
+        String command = splitMessage[1].replace(prefix,"");
+        String[] args = Arrays.stream(splitMessage).skip(2).toArray(String[]::new);
+
+        if(!message.startsWith(sender + " " + prefix)) {
+            //FishingBot.getLog().info("Mesage was not a command.");
+            return;
+        }
+
+        sender = sender.replaceAll("<", "").replaceAll(">", "");
+
+        if (command.equals("level")) {
+            FishingBot.getInstance().getNet().sendPacket(new PacketOutChat("I have " + FishingBot.getInstance().getPlayer().getLevels() + " levels."));
+        }
+
+        if (command.equals("health")) {
+            FishingBot.getInstance().getNet().sendPacket(new PacketOutChat("I am on " + FishingBot.getInstance().getPlayer().getHealth() + "health!"));
+        }
+
+        if(command.equals("say")) {
+            String saythis = String.join(" ", args);
+            FishingBot.getInstance().getNet().sendPacket(new PacketOutChat(saythis));
+        }
+
+        if (command.equals("stop")) {
+            if(!sender.equals(FishingBot.getInstance().getConfig().getBotOwner())) {
+                FishingBot.getInstance().getNet().sendPacket(new PacketOutChat("You don't have the permission to use this command!"));
+                return;
+            }
+            FishingBot.getInstance().getNet().sendPacket(new PacketOutChat("ok bye"));
+            new Thread(() -> {
+                try {
+                    Thread.sleep(150);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                FishingBot.getLog().info("Disconnecting...");
+                try {
+                    FishingBot.getInstance().getSocket().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Thread.sleep(150);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                FishingBot.getLog().info("Bot stopped.");
+                System.exit(0);
+            }).start();
         }
     }
 }
